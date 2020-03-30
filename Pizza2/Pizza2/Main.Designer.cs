144a25eb8059ec365074034e890adf576d3ee19b
Goedning <62235515+Goedning@users.noreply.github.com>
2020-03-30 14:31:27 +0200

    Nice
    Yeet

--- Pizza2/Pizza2/Main.Designer.cs
+++ Pizza2/Pizza2/Main.Designer.cs
@@ -47,6 +47,7 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // checkBox1
@@ -117,6 +118,7 @@
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Skinke Pizza";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton3
             // 
@@ -128,10 +130,11 @@
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Vegetar med Kød Pizza";
             this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 318);
+            this.button1.Location = new System.Drawing.Point(25, 330);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 37);
             this.button1.TabIndex = 9;
@@ -187,12 +190,13 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(25, 275);
+            this.button2.Location = new System.Drawing.Point(23, 244);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 37);
             this.button2.TabIndex = 15;
             this.button2.Text = "TILFØJ ORDRE";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // checkBox3
             // 
@@ -203,6 +207,7 @@
             this.checkBox3.TabIndex = 16;
             this.checkBox3.Text = "Normal";
             this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
             // 
             // label7
             // 
@@ -231,11 +236,22 @@
             this.label9.TabIndex = 19;
             this.label9.Text = "+10 kr";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 287);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 37);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "BESTIL ORDRE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(586, 379);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -284,6 +300,7 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
     }
 }
 
