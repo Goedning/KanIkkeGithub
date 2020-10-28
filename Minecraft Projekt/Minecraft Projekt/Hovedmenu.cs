c982be22d78be3598d7b196594f9d9c53636b59a
Goedning <62235515+Goedning@users.noreply.github.com>
2020-10-28 14:53:44 +0100

    1
    1

---
+++ Minecraft Projekt/Minecraft Projekt/Hovedmenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft_Projekt
+{
+    class Hovedmenu
+    {
+        string input = "";
+        public void Start()
+        {
+            Console.WriteLine("Velkommen til mit Minecraft projekt!");
+            Console.WriteLine("Hvis du er klar så bekræft venligst på y tasten");
+
+
+            input = Console.ReadLine();
+
+            if (input == "y")
+            {
+
+            }
+            else if (input == "n")
+            {
+                Environment.Exit(1);
+            }
+
+        }
+
+    }
+}
---
+++ Minecraft Projekt/Minecraft Projekt/MainStart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft_Projekt
+{
+    class MainStart
+    {
+        static void Main(string[] args)
+        {
+            Hovedmenu ui = new Hovedmenu();
+            ui.Start();
+        }
+    }
+}
